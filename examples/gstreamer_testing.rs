c2ba51029fa9d30886b61a859a54af3621053de9
Gamil Rodriguez Rodriguez <Gamil.Rodriguez@citrix.com>
2025-02-06 17:12:51 -0500

    refactor

--- examples/gstreamer_testing.rs
+++ examples/gstreamer_testing.rs
@@ -1,10 +1,14 @@
+use std::thread;
 
 use anyhow::Result;
-use gst::{prelude::*, Element, ElementFactory, Pipeline};
+use gst::{
+    glib::{self, MainLoop},
+    prelude::*,
+    Element, ElementFactory, Pipeline,
+};
 use gstreamer as gst;
-use log::error;
 
-fn create_pipeline() -> Result<gst::Pipeline> {
+fn create_pipeline(main_loop: MainLoop) -> Result<()> {
     gst::init()?;
 
     let pipeline = Pipeline::default();
@@ -16,40 +20,74 @@ fn create_pipeline() -> Result<gst::Pipeline> {
 
     Element::link_many(&[src, videoconv, sink])?;
 
-    Ok(pipeline)
-}
-
-fn main_loop(pipeline: gst::Pipeline) -> Result<()> {
     pipeline.set_state(gst::State::Playing)?;
 
-    let bus = pipeline.bus().expect("Pipeline without bus. Shouldn't happen!");
+    let bus = pipeline.bus().unwrap();
 
-    for msg in bus.iter_timed(gst::ClockTime::NONE) {
-        use gst::MessageView;
+    let main_loop_clone = main_loop.clone();
 
-        match msg.view() {
-            MessageView::Eos(..) => break,
-            MessageView::Error(err) => {
-                pipeline.set_state(gst::State::Null)?;
-                error!("Error {} in pipeline", err);
-            }
-            _ => (),
-        }
-    }
+    let _bus_watch = bus
+        .add_watch(move |_, msg| {
+            use gst::MessageView;
+
+            let main_loop = &main_loop_clone;
+            match msg.view() {
+                MessageView::Eos(..) => {
+                    println!("received eos");
+                    // An EndOfStream event was sent to the pipeline, so we tell our main loop
+                    // to stop execution here.
+                    main_loop.quit()
+                }
+                MessageView::Error(err) => {
+                    println!(
+                        "Error from {:?}: {} ({:?})",
+                        err.src().map(|s| s.path_string()),
+                        err.error(),
+                        err.debug()
+                    );
+                    main_loop.quit();
+                }
+                _ => (),
+            };
+
+            // Tell the mainloop to continue executing this callback.
+            glib::ControlFlow::Continue
+        })
+        .expect("Failed to add bus watch");
+
+    println!("Running the main loop");
+    main_loop.run();
 
     pipeline.set_state(gst::State::Null)?;
+
+    println!("End of the main loop");
 
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    tokio::spawn(async {
-        match create_pipeline().and_then(main_loop) {
-            Ok(r) => r,
-            Err(e) => eprintln!("Error! {e}"),
-        }
+    let main_loop = glib::MainLoop::new(None, false);
+
+    let main_loop_clone = main_loop.clone();
+
+    thread::spawn(move || match create_pipeline(main_loop_clone) {
+        Ok(r) => r,
+        Err(e) => eprintln!("Error! {e}"),
     });
 
-    println!("Running the main loop");
+    let mut seconds_to_die = 10;
+
+    while seconds_to_die > 0 {
+        println!(
+            "Waiting for the pipeline to finish: {} seconds left",
+            seconds_to_die
+        );
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        seconds_to_die -= 1;
+    }
+
+    println!("Stopping the main loop from the main thread");
+    main_loop.quit();
+    println!("Main loop stopped");
 }
